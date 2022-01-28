a830e0387ff2137c8002b263bf4dd1708ba0af6a
jgandrade <johnglenn.andrade@gmail.com>
2022-01-28 17:03:31 +0800

    Complete Navbar Functionality

---
+++ public/mobile-burger.js
@@ -0,0 +1,16 @@
+let burgerBtn = document.querySelector(".burger-btn");
+let navMenu = document.querySelector(".nav-menu");
+
+burgerBtn.addEventListener('click', () => {
+    navMenu.classList.remove('nav-menu-none');
+    navMenu.classList.add('nav-menu-block');
+});
+
+let closeBtn = document.querySelector('.close');
+
+closeBtn.addEventListener('click', ()=>{
+    navMenu.classList.add('nav-menu-none');
+    navMenu.classList.remove('nav-menu-block');
+})
+
+console.log(burgerBtn);
